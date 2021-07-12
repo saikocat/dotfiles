33338d63578a1963acc5429d02d9f2f7b2f339e5
Hoa <hoameomu@gmail.com>
2021-07-12 18:35:53 +0800

    [fish] update right prompt to use official prompt

--- fishshell/.config/fish/functions/fish_right_prompt.fish
+++ fishshell/.config/fish/functions/fish_right_prompt.fish
@@ -1,29 +1,23 @@
-
-function get_git_status -d "Gets the current git status"
-  if command git rev-parse --is-inside-work-tree >/dev/null 2>&1
-    set -l dirty (command git status -s --ignore-submodules=dirty | wc -l | sed -e 's/^ *//' -e 's/ *$//' 2> /dev/null)
-    set -l ref (command git describe --tags --exact-match ^/dev/null ; or command git symbolic-ref --short HEAD 2> /dev/null ; or command git rev-parse --short HEAD 2> /dev/null)
-
-    if [ "$dirty" != "0" ]
-      set_color -b normal
-      set_color red
-      echo "$dirty changed file"
-      if [ "$dirty" != "1" ]
-        echo "s"
-      end
-      echo " "
-      set_color -b red
-      set_color white
-    else
-      set_color -b green
-      set_color white
-    end
-
-    echo " $ref "
-    set_color normal
-   end
-end
-
 function fish_right_prompt -d "Prints right prompt"
-  get_git_status
+  set -g __fish_git_prompt_show_informative_status
+  set -g __fish_git_prompt_showcolorhints
+  set -g __fish_git_prompt_showupstream "informative"
+
+  set -g __fish_git_prompt_color_prefix green --bold
+  set -g __fish_git_prompt_color_suffix green --bold
+  set -g __fish_git_prompt_color_branch green --bold
+  set -g __fish_git_prompt_color_branch_detached red --bold
+  set -g __fish_git_prompt_color_cleanstate green --bold
+
+  set -g __fish_git_prompt_char_upstream_ahead "↑"
+  set -g __fish_git_prompt_char_upstream_behind "↓"
+  set -g __fish_git_prompt_char_upstream_prefix ""
+
+  set -g __fish_git_prompt_char_stagedstate "●"
+  set -g __fish_git_prompt_char_dirtystate "✚"
+  set -g __fish_git_prompt_char_untrackedfiles "…"
+  set -g __fish_git_prompt_char_conflictedstate "✖"
+  set -g __fish_git_prompt_char_cleanstate "✔"
+
+  fish_git_prompt
 end
