ddb29a7bc7c36766e73b3bba718d8f9baecb3e60
Hoa <hoameomu@gmail.com>
2024-10-23 00:00:31 +0800

    [neovim] show parent paths in lualine

--- neovim/.config/nvim/lua/plugins/user_interface/statusline.lua
+++ neovim/.config/nvim/lua/plugins/user_interface/statusline.lua
@@ -2,9 +2,25 @@ return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   opts = {
-      options = {
-        icons_enabled = true,
-        theme = 'ayu',
+    options = {
+      icons_enabled = true,
+      theme = 'ayu',
+    },
+    sections = {
+      lualine_c = {
+        {
+          'filename',
+          path = 3,
+        },
       },
     },
+    inactive_sections = {
+      lualine_c = {
+        {
+          'filename',
+          path = 3,
+        },
+      },
+    },
+  },
 }
