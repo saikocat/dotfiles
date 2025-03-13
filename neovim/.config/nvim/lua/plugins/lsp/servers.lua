d042b8946449b288090d99916d52dd3329ae0210
Hoa <hoameomu@gmail.com>
2025-03-13 13:57:25 +0800

    [neovim] fix undefined vim in lua

--- neovim/.config/nvim/lua/plugins/lsp/servers.lua
+++ neovim/.config/nvim/lua/plugins/lsp/servers.lua
@@ -38,6 +38,7 @@ return {
         },
         -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
         -- diagnostics = { disable = { 'missing-fields' } },
+        diagnostics = { globals = { 'vim' } },
       },
     },
   },
