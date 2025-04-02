e81f55b73a37e8c74d320742d3e9dd59f0d99d61
Hoa <hoameomu@gmail.com>
2025-04-02 11:04:03 +0800

    [nvim] diagnostic cfg to reduce cluttered virtual line

--- neovim/.config/nvim/lua/prelude/diagnostic.lua
+++ neovim/.config/nvim/lua/prelude/diagnostic.lua
@@ -1,10 +1,19 @@
 vim.diagnostic.config {
   -- Use the default configuration
-  virtual_lines = true,
+  -- virtual_lines = true,
 
   -- Alternatively, customize specific options
-  -- virtual_lines = {
-  --  -- Only show virtual line diagnostics for the current cursor line
-  --  current_line = true,
-  -- },
+  signs = { priority = 9999 },
+  underline = true,
+  update_in_insert = false, -- false so diags are updated on InsertLeave
+  virtual_text = { current_line = true, severity = { min = 'INFO', max = 'WARN' } },
+  virtual_lines = { current_line = true, severity = { min = 'ERROR' } },
+  severity_sort = true,
+  float = {
+    focusable = false,
+    style = 'minimal',
+    border = 'rounded',
+    source = true,
+    header = '',
+  },
 }
