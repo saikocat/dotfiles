a5fb525549152ae2a0b87ced267b9ea2151dd4b7
Hoa <hoameomu@gmail.com>
2025-04-05 20:13:41 +0700

    [neovim] use vim.o instead of opt for cursorline

--- neovim/.config/nvim/lua/prelude/options.lua
+++ neovim/.config/nvim/lua/prelude/options.lua
@@ -239,6 +239,6 @@ vim.o.diffopt = append({
 -- }}}
 
 -- Show which line your cursor is on
-vim.opt.cursorline = true
+vim.o.cursorline = true
 
 -- vim: ts=2 sts=2 sw=2 et foldenable foldlevel=1 foldmethod=marker
