04769e3e447ed3ca8c3f11a45aa5b5e02f701703
Hoa <hoameomu@gmail.com>
2025-04-06 20:26:57 +0800

    [nvim] statusline - add progress_as_scrollbar experiment

--- neovim/.config/nvim/lua/plugins/user_interface/statusline.lua
+++ neovim/.config/nvim/lua/plugins/user_interface/statusline.lua
@@ -1,3 +1,26 @@
+local function progress_as_scrollbar()
+  local sbar_chars = {
+    '▔',
+    '🮂',
+    '🬂',
+    '🮃',
+    '▀',
+    '▄',
+    '▃',
+    '🬭',
+    '▂',
+    '▁',
+  }
+
+  local cur_line = vim.api.nvim_win_get_cursor(0)[1]
+  local lines = vim.api.nvim_buf_line_count(0)
+
+  local i = math.floor((cur_line - 1) / lines * #sbar_chars) + 1
+  local sbar = string.rep(sbar_chars[i], 2)
+
+  return '%#' .. 'Substitute' .. '#' .. sbar .. '%*'
+end
+
 return {
   'nvim-lualine/lualine.nvim',
   dependencies = { 'nvim-tree/nvim-web-devicons' },
@@ -12,6 +35,11 @@ return {
           'filename',
           path = 3,
         },
+      },
+      lualine_y = {
+        function()
+          return progress_as_scrollbar()
+        end,
       },
     },
     inactive_sections = {
