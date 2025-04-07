df52e3cc9452ee741c884560ad09868a14cef031
Hoa <hoameomu@gmail.com>
2025-04-07 15:43:19 +0700

    [neovim] always recompile fro completion blink

--- neovim/.config/nvim/lua/plugins/completion_blink.lua
+++ neovim/.config/nvim/lua/plugins/completion_blink.lua
@@ -6,7 +6,7 @@ return {
     dependencies = 'rafamadriz/friendly-snippets',
 
     -- use a release tag to download pre-built binaries
-    version = '*',
+    -- version = '*',
     -- OR build from source, requires nightly: https://rust-lang.github.io/rustup/concepts/channels.html#working-with-nightly-rust
     build = 'cargo build --release',
 
