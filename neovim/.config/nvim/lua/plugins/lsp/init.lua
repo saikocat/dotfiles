e12097eff181daf1edf475357e597d4dc0563315
Hoa <hoameomu@gmail.com>
2025-04-12 10:52:37 +0700

    [neovim] use gR for reference goto to avoid conflicts w grr in v0.11

--- neovim/.config/nvim/lua/plugins/lsp/init.lua
+++ neovim/.config/nvim/lua/plugins/lsp/init.lua
@@ -67,7 +67,7 @@ return {
           map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
 
           -- Find references for the word under your cursor.
-          map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+          map('gR', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
 
           -- Jump to the implementation of the word under your cursor.
           --  Useful when your language has ways of declaring types without an actual implementation.
