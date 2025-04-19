1405b54d3feced67f2921ba864dadb17458d955f
Hoa <hoameomu@gmail.com>
2025-04-19 18:25:26 +0700

    [nvim] use gofumpt for golang conform

--- neovim/.config/nvim/lua/plugins/code/conform.lua
+++ neovim/.config/nvim/lua/plugins/code/conform.lua
@@ -34,7 +34,7 @@ return {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         python = { 'isort', 'black' },
-        go = { 'goimports', 'gofmt' },
+        go = { 'goimports', 'goimports_reviser', 'gofumpt' },
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         -- javascript = { { "prettierd", "prettier" } },
